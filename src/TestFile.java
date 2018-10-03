c83bd55a0c767f719e43bff37c5daf48b44dd751
Lin Yao Pan <lpan7932@bths.edu>
2018-10-03 14:49:23 -0400

    One New Method.

--- src/TestFile.java
+++ src/TestFile.java
@@ -1,7 +1,7 @@
 public class TestFile {
     public static void main(String[] args)
     {
-        System.out.println(LPanLib.dateStr("10/02/2018") );
+        System.out.println(LPanLib.isFibonnaci(13) );
     }
 
 }
